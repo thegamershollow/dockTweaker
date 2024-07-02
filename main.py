9091a48b92918bed54ee76c600a9ddf13f02e928
Ethan <thegamershollow@outlook.com>
2024-07-02 12:42:54 +0000

    removed hide spotlight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
-def hideSpotlight():
-    os.system(" sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search; killall SystemUIServer")
+#def hideSpotlight():
+#    os.system("sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search; killall SystemUIServer")
 
 def hiddenAppTransparency():
     os.system("defaults write com.apple.dock showhidden -bool TRUE; killall Dock")
@@ -15,15 +15,15 @@ def addLargeSpacer():
 def fullPathTitleBar():
     os.system(" defaults write com.apple.finder _FXShowPosixPathInTitle -bool TRUE")
 
-prompt = input("Mac Tweaker!\n1. Hide Spotlight\n2. Transparency for hidden apps in dock\n3. Add small spacer to dock\n4. Add large spacer to dock\n5. Full path in finder title bar\n6. Exit")
+prompt = input("Mac Tweaker!\n1. Transparency for hidden apps in dock\n2. Add small spacer to dock\n3. Add large spacer to dock\n4. Full path in finder title bar\n5. Exit\n: ")
 
+#if prompt == "1":
+    #hideSpotlight()
 if prompt == "1":
-    hideSpotlight()
-elif prompt == "2":
     hiddenAppTransparency()
-elif prompt == "3":
+elif prompt == "2":
     addSmallSpacer()
-elif prompt == "4":
+elif prompt == "3":
     addLargeSpacer()
-elif prompt == "5":
+elif prompt == "4":
     fullPathTitleBar()
